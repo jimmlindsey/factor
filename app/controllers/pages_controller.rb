942d0e90089ec2e57c31c2aa94c3e7f6ec7b68d9
Jimmy Lindsey <bkjimmie@gmail.com>
2017-07-23 09:41:27 +0200

    add pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,79 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def knowledgecenter
+  end
+
+  def howitworks
+  end
+
+  def infrastructure
+  end
+
+  def defensecontracts
+  end
+
+  def importexport
+  end
+
+  def agriculture
+  end
+
+  def construction
+  end
+
+  def textile
+  end
+
+  def startups
+  end
+
+  def team
+  end
+
+  def press
+  end
+
+  def jobs
+  end
+
+  def sell
+  end
+
+  def buy
+  end
+
+  def regulationa
+  end
+
+  def factoring101
+  end
+
+  def equitycrowdfunding
+  end
+
+  def whitepapers
+  end
+
+  def blog
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def legaldocs
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,56 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/faq'
+
+  get 'pages/knowledgecenter'
+
+  get 'pages/howitworks'
+
+  get 'pages/infrastructure'
+
+  get 'pages/defensecontracts'
+
+  get 'pages/importexport'
+
+  get 'pages/agriculture'
+
+  get 'pages/construction'
+
+  get 'pages/textile'
+
+  get 'pages/startups'
+
+  get 'pages/team'
+
+  get 'pages/press'
+
+  get 'pages/jobs'
+
+  get 'pages/sell'
+
+  get 'pages/buy'
+
+  get 'pages/regulationa'
+
+  get 'pages/factoring101'
+
+  get 'pages/equitycrowdfunding'
+
+  get 'pages/whitepapers'
+
+  get 'pages/blog'
+
+  get 'pages/terms'
+
+  get 'pages/privacy'
+
+  get 'pages/legaldocs'
 end
