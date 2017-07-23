6eb9d2de8c450f83086dbefd1b14d8de047ff738
Jimmy Lindsey <bkjimmie@gmail.com>
2017-07-23 09:27:43 +0200

    add avatars to users, update application controller, update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,5 +6,8 @@ class User < ApplicationRecord
   
   validates :name, presence: true, length: {maximum: 50}
   validates :company, presence: true, length: {maximum: 50}
-  validates :amount, presence: true
+  validates :sales, presence: true
+  
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723070742) do
+ActiveRecord::Schema.define(version: 20170723072052) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(version: 20170723070742) do
     t.string   "phone_number"
     t.string   "title"
     t.string   "address"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
