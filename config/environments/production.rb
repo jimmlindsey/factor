66983bcf63884ac10d590953e5a9cfb8ea969898
Jimmy Lindsey <bkjimmie@gmail.com>
2017-07-25 02:48:11 +0200

    update host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,5 +86,5 @@ Rails.application.configure do
   
   # Required for Heroku
   # Note to set this to your actual host
-  config.action_mailer.default_url_options = { host: 'https://arcane-hamlet-77765.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://arcane-hamlet-77765.herokuapp.com' }
 end
