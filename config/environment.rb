570785a015f03a09503f06507bf3ea2f6269a20d
Jimmy Lindsey <bkjimmie@gmail.com>
2017-07-25 02:00:42 +0200

    add sendgrid to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'factoring',
+  :password => 'Foamposite1',
+  :domain => 'https://arcane-hamlet-77765.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
