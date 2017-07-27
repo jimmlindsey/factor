2c8c2eaf0d2441ef20ce2670ade86b04642eaecc
Jimmy Lindsey <bkjimmie@gmail.com>
2017-07-27 16:55:08 +0200

    Revert "update host"
    This reverts commit 66983bcf63884ac10d590953e5a9cfb8ea969898.

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'app73338130@heroku.com',
   :password => '61zjngh05036',
-  :domain => 'https://arcane-hamlet-77765.herokuapp.com',
+  :domain => 'https://arcane-hamlet-77765.herokuapp.com/',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
